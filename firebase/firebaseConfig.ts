2057fefb88ee55b4062b87c700025be372503e94
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2022-04-03 19:32:20 +0100

    testing first conection in root

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -1,15 +1,21 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp, applicationDefault } from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
+
 import dotenv from 'dotenv'
 dotenv.config()
 
-console.log(process.env.API_KEY)
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
-}
+// const firebaseConfig = {
+//   apiKey: process.env.API_KEY,
+//   authDomain: process.env.AUTH_DOMAIN,
+//   projectId: process.env.PROJECT_ID,
+//   storageBucket: process.env.STORAGE_BUCKET,
+//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//   appId: process.env.APP_ID
+// }
 
-const app = initializeApp(firebaseConfig)
+// initializeApp(firebaseConfig)
+initializeApp({
+  credential: applicationDefault()
+})
+
+export const db = getFirestore()
