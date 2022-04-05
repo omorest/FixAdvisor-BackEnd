7d40c7ca9834d89b4ee33cf0172336d6b6a44592
Oscar Moreira <oscar.moreira.estevez@gmail.com>
2022-04-05 13:47:32 +0100

    test gets

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Express } from 'express'
 import { router } from './routes'
+import bodyParser from 'body-parser'
 
 const app: Express = express()
 
 app.use(router)
+app.use(bodyParser.json())
 
 export { app }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,6 @@ import { db } from '../../firebase/firebaseConfig'
 const router = Router()
 
 router.get('/', async (req: Request, res: Response) => {
-  const usersDocs = await db.collection('users').get()
-  console.log(usersDocs.docs[0].data())
   res.send('<h1>Fix Advisor</h1>')
 })
 
@@ -17,8 +15,50 @@ router.get('/api/users/', async (req: Request, res: Response) => {
       ...doc.data()
     }
   })
-  console.log(users)
-  res.send('<h1>Fix Advisor</h1>')
+  res.json(users)
+})
+
+router.get('/api/users/clients', async (req: Request, res: Response) => {
+  const usersDocs = await db.collection('users').get()
+  const clients = usersDocs.docs.filter(doc => doc.data().type === 'client').map(doc => {
+    return {
+      id: doc.id,
+      ...doc.data()
+    }
+  })
+  res.json(clients)
+})
+
+router.get('/api/users/providers', async (req: Request, res: Response) => {
+  const usersDocs = await db.collection('users').get()
+  const providers = usersDocs.docs.filter(doc => doc.data().type === 'provider').map(doc => {
+    return {
+      id: doc.id,
+      ...doc.data()
+    }
+  })
+  res.json(providers)
+})
+
+router.post('api/users/new-client', async (req: Request, res: Response) => {
+  const { name, type, email } = req.body
+  await db.collection('users').add({
+    name,
+    type,
+    email
+  })
+  res.json({ status: 200 })
+})
+
+router.get('/api/services/', async (req: Request, res: Response) => {
+  const usersDocs = await db.collection('services').get()
+  const services = usersDocs.docs.map(doc => {
+    return {
+      id: doc.id,
+      ...doc.data()
+    }
+  })
+  res.json(services)
 })
 
 export { router }
