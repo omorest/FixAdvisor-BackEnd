80f24dce03e705f6368f8a1b9a4f4482929c7004
omorest <oscar.moreira.estevez@gmail.com>
2022-06-19 17:22:54 +0100

    fetch get favourites

--- src/models/client.model.ts
+++ src/models/client.model.ts
@@ -2,4 +2,5 @@ export interface Client{
   name: string
   email: string
   id: string
+  favouriteServices?: string[]
 }
--- src/routes/users/users.service.ts
+++ src/routes/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express'
 import { db } from '../../../firebase/firebaseConfig'
+import { Service } from '../../models'
 
 const routerUsers = Router()
 
@@ -26,6 +27,18 @@ routerUsers.get('/api/users/:id', async (req: Request, res: Response) => {
   res
     .status(400)
     .json({ message: "user doesn't exist" })
+})
+
+routerUsers.get('/api/favourites/:idClient', async (req: Request, res: Response) => {
+  const { idClient } = req.params
+  const clientsRef = db.collection('clients')
+  const client = await clientsRef.where('id', '==', idClient).get()
+  const { favouriteServices } = client.docs[0].data()
+
+  const servicesRef = await db.collection('services').get()
+  const allServices = servicesRef.docs.map(doc => doc.data())
+  const services = allServices.filter((service: Service) => favouriteServices.map(id => id === service.id))
+  res.json(services)
 })
 
 export { routerUsers }
