60cd19b4c7c124d99cc2a422a8d5145ce2a5330b
omorest <oscar.moreira.estevez@gmail.com>
2022-07-07 10:26:44 +0100

    Change enviroment variables

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -5,19 +5,19 @@ import { getAuth } from 'firebase-admin/auth'
 import dotenv from 'dotenv'
 dotenv.config()
 
-// const firebaseConfig = {
-//   apiKey: process.env.API_KEY,
-//   authDomain: process.env.AUTH_DOMAIN,
-//   projectId: process.env.PROJECT_ID,
-//   storageBucket: process.env.STORAGE_BUCKET,
-//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
-//   appId: process.env.APP_ID
-// }
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+}
 
-// initializeApp(firebaseConfig)
-const defaultApp = initializeApp({
-  credential: applicationDefault()
-})
+const defaultApp = initializeApp(firebaseConfig)
+// const defaultApp = initializeApp({
+//   credential: applicationDefault()
+// })
 
 export const authF = getAuth(defaultApp)
 export const db = getFirestore()
